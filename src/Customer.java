5d6850f4c39e9164573a3b169c79536dcecd419f
Emily Bowe <ebowe@tcd.ie>
2020-03-24 22:02:06 +0000

    make Customer class - include field, constructor, instances

---
+++ src/Customer.java
@@ -0,0 +1,14 @@
+public class Customer {
+
+    private String name;
+
+    Customer (String name) {
+        this.name = name;
+    }
+
+    public static final Customer JACK = new Customer("Jack");
+    public static final Customer JILL = new Customer("Jill");
+    public static final Customer Mary = new Customer("Mary");
+
+
+}
