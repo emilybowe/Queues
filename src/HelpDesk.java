038bbcd0179f98b0f750ff1be00c445a9cf8c7b6
Emily Bowe <ebowe@tcd.ie>
2020-03-25 17:24:09 +0000

    add to Helpdesk - ArrayDeque, method implementation (enquire and ProcessAllEnquiries)

--- src/HelpDesk.java
+++ src/HelpDesk.java
@@ -1,11 +1,15 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class HelpDesk {
 
+    private final Queue<Enquiry> enquires = new ArrayDeque<>();
     public void enquire(final Customer customer, final Category category) {
-
+        enquires.offer(new Enquiry(Customer.JACK, Category.PRINTER));
     }
 
     public void processAllEnquires() {
-
+        final Enquiry enquiry = enquires.poll();
     }
 
 }
