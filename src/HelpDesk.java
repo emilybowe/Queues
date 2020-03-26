d459b50c3356fddc25aed2f25f8868939d41698e
Emily Bowe <ebowe@tcd.ie>
2020-03-26 12:24:55 +0000

    Housekeeping - make code & output more readable

--- src/HelpDesk.java
+++ src/HelpDesk.java
@@ -4,13 +4,14 @@ import java.util.Queue;
 public class HelpDesk {
 
     private final Queue<Enquiry> enquires = new ArrayDeque<>();
-    public void enquire(final Customer customer, final Category category) {
+
+    public void enquire(Customer customer, Category category) {
         enquires.offer(new Enquiry(Customer.JACK, Category.PRINTER));
     }
 
     public void processAllEnquires() {
         final Enquiry enquiry = enquires.poll();
-        enquiry.getCustomer().reply("turn off and on again");
+        enquiry.getCustomer().reply(": turn off and on again");
     }
 
 }
