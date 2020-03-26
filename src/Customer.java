37feda648c4ce8ad7479ea542d474c45a25846f1
Emily Bowe <ebowe@tcd.ie>
2020-03-26 10:28:56 +0000

    Customer - add reply method, tidy up name

--- src/Customer.java
+++ src/Customer.java
@@ -5,10 +5,12 @@ public class Customer {
     Customer (String name) {
         this.name = name;
     }
-
     public static final Customer JACK = new Customer("Jack");
     public static final Customer JILL = new Customer("Jill");
-    public static final Customer Mary = new Customer("Mary");
+    public static final Customer MARY = new Customer("Mary");
 
+    public void reply(final String message) {
+        System.out.printf(name, message);
+    }
 
 }
