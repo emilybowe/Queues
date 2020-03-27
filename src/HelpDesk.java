3af00774d23827960c379bfd314d183d1fa074fc
Emily Bowe <ebowe@tcd.ie>
2020-03-27 23:59:06 +0000

    Implement comparator

--- src/HelpDesk.java
+++ src/HelpDesk.java
@@ -1,9 +1,15 @@
 import java.util.ArrayDeque;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class HelpDesk {
-
+    private static final Comparator<Enquiry>BY_CATEGORY = new Comparator<Enquiry>() {
+        @Override
+        public int compare(Enquiry o1, Enquiry o2) {
+            return o1.getCategory().compareTo(o2.getCategory());
+        }
+    };
     private final Queue<Enquiry> enquires = new PriorityQueue<>();
 
     public void enquire(Customer customer, Category category) {
